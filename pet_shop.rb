9dc131e9439ab11ace2c6240fef48d8219eb24af
Paul Kelly <paul.kelly20@gmail.com>
2018-03-30 11:51:00 +0100

    passed fifth test

--- pet_shop.rb
+++ pet_shop.rb
@@ -5,3 +5,13 @@ end
 def total_cash(pet_shop)
   return pet_shop[:admin][:total_cash]
 end
+
+def add_or_remove_cash(pet_shop,cash)
+  return pet_shop[:admin][:total_cash] += cash
+  return pet_shop[:admin][:total_cash] -= cash
+
+end
+
+def pets_sold(pet_shop)
+  return pet_shop[:admin][:pets_sold]
+end
